6a8923f3c18577e352eed665790f55b813083bcb
alMohamady <al.Mohamady47@gmail.com>
2019-06-01 16:16:15 +0300

    Row, Column and Expanded widget

--- lib/layouts/home_screen.dart
+++ lib/layouts/home_screen.dart
@@ -1,23 +1,79 @@
 import 'package:flutter/material.dart';
 
-
 class HomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
-       child:  Container(
+        child: Container(
+            padding: EdgeInsets.only(top: 40, left: 10, right: 10, bottom: 20),
             alignment: Alignment.center,
             color: Colors.yellow,
-            child: Text("Hi Flutter",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 70,
-                fontFamily: "Arimo",
-                fontWeight: FontWeight.w700,
-              ),
+            child: Column (
+               children: <Widget>[
+                 Row(
+                     textDirection: TextDirection.ltr,
+                     children: <Widget>[
+                       Expanded(
+                           child: Text(
+                             "Ahmed",
+                             textDirection: TextDirection.ltr,
+                             style: TextStyle(
+                               color: Colors.black,
+                               backgroundColor: Colors.green,
+                               fontSize: 20,
+                               fontFamily: "Arimo",
+                               fontWeight: FontWeight.w700,
+                             ),
+                           )
+                       ),
+                       Expanded(
+                           child: Text(
+                               "Senior Software Developer in mobile apps",
+                               textDirection: TextDirection.ltr,
+                               style: TextStyle(
+                                 color: Colors.black,
+                                 backgroundColor: Colors.lightBlueAccent,
+                                 fontSize: 20,
+                                 fontFamily: "Arimo",
+                                 fontWeight: FontWeight.w700,
+                               )
+                           )
+                       )
+                     ]
+                 ),
+                 Row(
+                     textDirection: TextDirection.ltr,
+                     children: <Widget>[
+                       Expanded(
+                           child: Text(
+                             "Mohamed",
+                             textDirection: TextDirection.ltr,
+                             style: TextStyle(
+                               color: Colors.black,
+                               backgroundColor: Colors.green,
+                               fontSize: 20,
+                               fontFamily: "Arimo",
+                               fontWeight: FontWeight.w700,
+                             ),
+                           )
+                       ),
+                       Expanded(
+                           child: Text(
+                               "Senior Software Analyzer",
+                               textDirection: TextDirection.ltr,
+                               style: TextStyle(
+                                 color: Colors.black,
+                                 backgroundColor: Colors.lightBlueAccent,
+                                 fontSize: 20,
+                                 fontFamily: "Arimo",
+                                 fontWeight: FontWeight.w700,
+                               )
+                           )
+                       )
+                     ]
+                 )
+               ]
             )
         )
     );
