9e0c100c01872389012c04e6f68d1688bb47a9d8
alMohamady <al.Mohamady47@gmail.com>
2019-07-19 20:14:44 +0300

    Add Basic List View

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'layouts/home_screen.dart';
 
 
-void main() => runApp(MyFlutterApp());
-
+//void main() => runApp(MyFlutterApp());
 
 class MyFlutterApp extends StatelessWidget{
 
@@ -13,6 +12,48 @@ class MyFlutterApp extends StatelessWidget{
     return  HomeScreen();
   }
 }
+
+void main() {
+  runApp(
+      MaterialApp(
+          title: "My List View",
+          home: Scaffold(
+            body: myListView(),
+          )
+      )
+  );
+}
+
+Widget myListView() {
+  var list = ListView(
+    children: <Widget>[
+
+      ListTile(
+        leading: Icon(Icons.access_alarm),
+        title: Text("My alarm"),
+        subtitle: Text("the body of the alam list"),
+        trailing: Icon(Icons.accessibility),
+        onTap: () {
+          debugPrint("Hello world");
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.adb),
+        title: Text("My android"),
+        subtitle: Text("the body of the android list"),
+        trailing: Icon(Icons.image),
+      ),      ListTile(
+        title: Text("My android"),
+        trailing: Icon(Icons.image),
+      ),
+
+    ],
+  );
+  return list;
+}
+
+
+
 
 
 
