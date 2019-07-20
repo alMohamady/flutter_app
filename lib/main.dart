d8a9daa0efc1b4c5c5ab7b0c2d0a68403059fa1a
alMohamady <al.Mohamady47@gmail.com>
2019-07-20 18:55:42 +0300

    Add StatefulWidget TextField

--- lib/main.dart
+++ lib/main.dart
@@ -13,25 +13,6 @@ class MyFlutterApp extends StatelessWidget{
   }
 }
 
-void main() {
-  runApp(
-      MaterialApp(
-          title: "My List View",
-          home: Scaffold(
-            appBar: AppBar(title: Text("The big List")),
-            body: myBigList(),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                debugPrint("Add new item");
-              },
-              child: Icon(Icons.add),
-              tooltip: "add new item",
-            ),
-          )
-      )
-  );
-}
-
 Widget myListView() {
   var list = ListView(
     children: <Widget>[
@@ -114,7 +95,56 @@ void onClick(BuildContext context){
   );
 }
 
+void main() {
+  runApp(
+    MaterialApp(
+      title: "the Stateful Widgets",
+      home: StudentsNames()
+    )
+  );
+}
 
+class StudentsNames extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _StudentsNames();
+  }
+}
+
+class _StudentsNames extends State<StudentsNames> {
+
+  String student;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Stateful project"),),
+      body:
+      Container(
+        margin: EdgeInsets.all(20),
+        child:  Column(
+           children: <Widget>[
+              TextField(
+                onChanged: (String userInput) {
+                  setState(() {
+                    student = userInput;
+                  });
+                },
+              ),
+              Padding(
+                padding:  EdgeInsets.all(30),
+                child:  Text("the Student is : $student")
+              )
+            ],
+          )
+      )
+      ,
+    );
+  }
+
+}
 
 
 
